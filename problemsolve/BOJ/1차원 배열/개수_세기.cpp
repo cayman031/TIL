89d14f1770b0f8efdd81316a87e2be893d5cef9b
cayman031 <cayman031@gmail.com>
2025-12-18 10:54:15 +0900

    feat: 백준 개수 세기

--- problemsolve/BOJ/1차원 배열/개수_세기.cpp
+++ problemsolve/BOJ/1차원 배열/개수_세기.cpp
@@ -6,26 +6,20 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
+    int arr[201] = {0};
     int n, v;
-    int ans = 0;
-    int arr[101];
 
-    cin >> n; 
-    
+    cin >> n;
+
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        int t;
+        cin >> t;
+        arr[t+100]++;
     }
+
     cin >> v;
+    cout << arr[v+100];
 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == v) {
-            ans++;
-        }
-    }
-
-    cout << ans;
-    
-    
 
     return 0;
 }
