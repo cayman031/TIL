7b53954a276435454070600522de7677a6c9249f
cayman031 <cayman031@gmail.com>
2025-12-22 11:22:50 +0900

    feat: 백준

---
+++ problemsolve/BOJ/리스트/에디터.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    list<char> l;
+    for (auto c:s) l.push_back(c);
+    auto cursor = l.end();
+
+    int m;
+    cin >> m;
+
+    while(m--) {
+        char op;
+        cin >> op;
+
+        if (op == 'L') {
+            if (cursor != l.begin()) cursor--;
+        } else if (op == 'D') {
+            if (cursor != l.end()) cursor++;
+        } else if (op == 'B') {
+            if (cursor != l.begin()) {
+                cursor--;
+                cursor = l.erase(cursor);
+            }
+        } else {
+            char add;
+            cin >> add;
+            l.insert(cursor,add);
+        }
+    }
+
+    for (auto ans:l) {
+        cout << ans;
+    }
+
+
+    return 0;
+}
